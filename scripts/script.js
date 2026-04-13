0e5f1889c40ba989cdb1f5bf391b725c05aafc8c
Amin <chogueuramine@gmail.com>
2026-04-13 23:51:20 +0200

    modife the script.js file in the staging branch

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,3 @@
 console.log(
-  "hello from the remote repo, this change has been made in the local machin ",
+  "hello from the remote repo, this change has been made in the local machin from the staging branch",
 );
