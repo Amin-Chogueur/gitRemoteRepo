f893f53cea35a1e0465f9f4c4c6db1a615aa940f
Amin <chogueuramine@gmail.com>
2026-04-14 00:46:09 +0200

    add anothre console in script

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
 console.log(
   "hello from the remote repo, this change has been made in the local machin from the staging branch",
 );
+console.log(
+  "hello from the remote repo, this change has been made in the local machin from the staging branch",
+);
